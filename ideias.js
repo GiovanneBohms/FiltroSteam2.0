71ffaa497264a34a7ca5a795d9bce41452f4f8fb
giovannebohms <giovannebohms@gmail.com>
2023-11-13 19:54:28 -0300

    ideias.js

--- ideias.js
+++ ideias.js
@@ -1,15 +1,24 @@
-let configuracao= {
+const configuracao = {
   // Config para coleta de API
-  "quantidadeParaPesquisar": 20000,
-  "ofertasMinimas": 5000,
-  "precoPrimario": 10.0,
+  quantidadeParaPesquisar: 5000,
+  ofertasMinimas: 5000,
+  precoPrimario: 10.0,
+  cotacaoDolar: 4.91,
   //Config após coleta de API
-  "horas": 336,
-  "volume": 10000,
-  "precoMinimo": 0.1,
-  "precoMaximo": 0.4,
+  horas: 336,
+  volume: 10000,
+  precoMinimo: 0.1,
+  precoMaximo: 0.4,
+};
+
+let item = {
+  precoDeVenda: 0.25,
+  ofertasListadas: 5000,
+  name: 'Dreams%20%26%20Nightmares%20Case',
+  id: 730,
+  cotacao: [],
+  idBook: "",
 };
 
 console.log("oi");
 console.log(configuracao.horas)
-helloByModule()
