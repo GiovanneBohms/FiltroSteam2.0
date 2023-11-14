09d496b0026518e8f4d72814c2ed0bf28b612781
giovannebohms <giovannebohms@gmail.com>
2023-11-14 15:23:04 -0300

    item in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const configuracao = {
   // Config para coleta de API
-  quantidadeParaPesquisar: 200,
+  quantidadeParaPesquisar: 100,
   ofertasMinimas: 1000,
   precoPrimario: 10.0,
   cotacaoDolar: 4.91,
@@ -9,4 +9,13 @@ const configuracao = {
   volume: 10000,
   precoMinimo: 0.1,
   precoMaximo: 0.4,
+};
+
+let item = {
+  precoDeVenda: 0.25,
+  ofertasListadas: 5000,
+  name: "Dreams%20%26%20Nightmares%20Case",
+  id: 730,
+  cotacao: [],
+  idBook: "",
 };
