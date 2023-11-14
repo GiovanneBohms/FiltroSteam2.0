388bf9ea9cf903be76e7c3bae2c7e9ee554f7ff7
giovannebohms <giovannebohms@gmail.com>
2023-11-14 15:23:15 -0300

    Regex in dependencias.js

--- src/dependencias.js
+++ src/dependencias.js
@@ -4,3 +4,15 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function regexIdNamePriceQuant(string){
+  const regex =
+    /steamcommunity\.com\/market\/listings\/(\d+)\/([^"]+)"[\s\S]*?data-qty="([^"]+)"[\s\S]*?data-price="([^"]+)"/g
+  let listaStringItens = string.matchAll(regex)
+  let listaStringItensArray = [...string.matchAll(regex)];
+  criaItem(listaStringItensArray)
+  return listaStringItens;
+}
+
+function criaItem(array){
+  console.log(array.length)
+}
