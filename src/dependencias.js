1795612222fc4e6dfe2ed1bf44fa16f9c0017083
giovannebohms <giovannebohms@gmail.com>
2023-11-14 16:21:09 -0300

    criação de item

--- src/dependencias.js
+++ src/dependencias.js
@@ -1,18 +1,23 @@
-console.log("./src/dependencias.js")
+console.log("./src/dependencias.js");
 
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function regexIdNamePriceQuant(string){
+function regexIdNamePriceQuant(string) {
   const regex =
-    /steamcommunity\.com\/market\/listings\/(\d+)\/([^"]+)"[\s\S]*?data-qty="([^"]+)"[\s\S]*?data-price="([^"]+)"/g
-  let listaStringItens = string.matchAll(regex)
+    /steamcommunity\.com\/market\/listings\/(\d+)\/([^"]+)"[\s\S]*?data-qty="([^"]+)"[\s\S]*?data-price="([^"]+)"/g;
+  let listaStringItens = string.matchAll(regex);
   let listaStringItensArray = [...string.matchAll(regex)];
-  criaItem(listaStringItensArray)
+  criaItem(listaStringItensArray);
   return listaStringItens;
 }
 
-function criaItem(array){
-  console.log(array.length)
+function criaItem(array) {
+  let itens = [];
+  for (let i = 0; i < array.length; i++) {
+    item = new Item(array[i][4], array[i][3], array[i][2], array[i][1]);
+    itens.push(item);
+  }
+  return itens;
 }
