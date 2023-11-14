41205f6fc201d73f1368c293a78ab410903c172a
giovannebohms <giovannebohms@gmail.com>
2023-11-13 21:52:50 -0300

    config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const configuracao = {
+  // Config para coleta de API
+  quantidadeParaPesquisar: 5000,
+  ofertasMinimas: 5000,
+  precoPrimario: 10.0,
+  cotacaoDolar: 4.91,
+  //Config após coleta de API
+  horas: 336,
+  volume: 10000,
+  precoMinimo: 0.1,
+  precoMaximo: 0.4,
+};
