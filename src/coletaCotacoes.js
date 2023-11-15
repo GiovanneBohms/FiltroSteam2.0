7448487d3ea457738a6f360feb37daa1d15d731c
giovannebohms <giovannebohms@gmail.com>
2023-11-15 18:37:40 -0300

    coletaCotações

---
+++ src/coletaCotacoes.js
@@ -0,0 +1,74 @@
+console.log("./src/coletaCotacoes.js");
+
+function regexCotacao(string) {
+  const regex = /\[("[^"]+"),(\d+\.\d+),"(\d+)"\]/g;
+  if (string == null) {
+    return null;
+  }
+  const matches = string.match(regex);
+  return matches;
+}
+
+function regexIdBook(string) {
+  const regex = /Market_LoadOrderSpread\(\s*(\d+)\s*\)/;
+  if (string == null) {
+    return null;
+  }
+  const match = string.match(regex);
+  if (match) {
+    const idBook = match[1];
+    return idBook;
+  }
+}
+
+function converteStringEmCotacoes(stringCotacoesEIdBook) {
+  const horas = configuracao.horas;
+  let cotacoes = [];
+  let arrayStringCotacoes = regexCotacao(stringCotacoesEIdBook);
+  arrayStringCotacoes = arrayStringCotacoes.slice(-horas);
+  for (let i = 0; i < arrayStringCotacoes.length; i++) {
+    let stringCotacao = arrayStringCotacoes[i];
+    const jsonStringSemEscape = stringCotacao.replace(/\\/g, "");
+    let cotacao = JSON.parse(jsonStringSemEscape);
+    cotacao[2] = parseInt(cotacao[2], 10);
+    cotacoes.push(cotacao);
+  }
+  return cotacoes;
+}
+
+async function converteStringEmCotacaoEIDBook() {
+  let itens = await coletaIntervalos();
+  let i = 0;
+
+  while (i < itens.length) {
+    let stringCotacoesEIdBook = await coletaCotacoesEIdBook(
+      itens[i].id,
+      itens[i].name
+    );
+    if (!stringCotacoesEIdBook) {
+      console.log("muitas requisições, aguarde 5 minutos");
+      await delay(1000 * 60 * 5);
+      continue;
+    }
+    let cotacoes = converteStringEmCotacoes(stringCotacoesEIdBook);
+    let idBook = regexIdBook(stringCotacoesEIdBook);
+    itens[i].setCotacoes(cotacoes);
+    itens[i].setIdBook(idBook);
+    itens[i].setLink();
+
+    console.log(i + 1, "de", itens.length, itens[i]);
+    i++;
+  }
+
+  abrirLinks(itens)
+}
+
+function abrirLinks(itens) {
+
+  for(let i = 0; i< itens.length;i++){
+    let link = itens[i].link;
+    window.open(link, "_blank");
+  }
+  console.log("fim");
+}
+converteStringEmCotacaoEIDBook();
