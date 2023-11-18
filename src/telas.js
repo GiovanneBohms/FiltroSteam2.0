298f28729e28308d1500636acef098151c7aada8
giovannebohms <giovannebohms@gmail.com>
2023-11-18 01:57:49 -0300

    oferta total na tela

--- src/telas.js
+++ src/telas.js
@@ -1,13 +1,18 @@
-console.log("./src/telas,js")
+console.log("./src/telas,js");
 
-async function renderizador(){
-  const telaInput = document.getElementById("popularItemsRows")
-  const maisItens = document.getElementById("popularItemsMore")
-  maisItens.innerHTML = ""
+async function renderizador() {
+  const telaInput = document.getElementById("popularItemsRows");
+  const maisItens = document.getElementById("popularItemsMore");
+  maisItens.innerHTML = "";
   telaInput.innerHTML = `
   <div id="printConfiguracao" style="max-width: 620px;"> </div> 
   <div id="printAtualizacoes" style="max-width: 620px;"> 
   <form id="meuFormulario" style="display: flex; flex-direction: column; max-width: 450px; margin: 16px auto 64px auto;">
+
+  <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+  <label for="caixa"><input type="checkbox" id="caixa" name="opcao" value="case"> Caixa</label>
+  <label for="cartas"><input type="checkbox" id="cartas" name="opcao" value="cartas"> cartas</label><br>
+  </div>
   <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
       <label for="quantItens">Quantidade de Itens para pesquisar:</label>
       <input type="number" id="quantItens" name="quantItens" value="100">
@@ -39,31 +44,31 @@ async function renderizador(){
   <div id="printItensFiltrados"></div>
   <p style="text-align: center; font-size: 12px; margin-top:40px;">By: Giovanne Bohms</p>
   
-`
+`;
 }
-renderizador()
-function telaAtualizações(string){
-const printAtualizacoes = document.getElementById("printAtualizacoes")
-const printConfig = document.getElementById("printConfiguracao")
+renderizador();
+function telaAtualizações(string) {
+  const printAtualizacoes = document.getElementById("printAtualizacoes");
+  const printConfig = document.getElementById("printConfiguracao");
 
-let printConfiguracao=`<span style="font-weight: bold;">Configuração definida:</span><br><br> Total a Pesquisar:  ${configuracao.quantidadeParaPesquisar}<br>
+  let printConfiguracao = `<span style="font-weight: bold;">Configuração definida:</span><br><br> Total a Pesquisar:  ${configuracao.quantidadeParaPesquisar}<br>
 Ofertas Minimas:  ${configuracao.ofertasMinimas}<br>
 Intervalo em Horas:  ${configuracao.horas}<br>
 Volume Médio por Hora:  ${configuracao.volumeMedioHora}<br>
 Preço Mínimo:  ${configuracao.precoMinimo}<br>
 Preço Mínimo:  ${configuracao.precoMaximo}<br><br>
-<span style="font-weight: 700;">Aguarde a coleta de dados:</span><br>`
+<span style="font-weight: 700;">Aguarde a coleta de dados:</span><br>`;
 
-let print=`<div style="max-width: 620px;">
+  let print = `<div style="max-width: 620px;">
 <p style="word-wrap: break-word;">${string}</p>
-</div>`
+</div>`;
 
-printConfig.innerHTML = printConfiguracao
-printAtualizacoes.innerHTML = print
+  printConfig.innerHTML = printConfiguracao;
+  printAtualizacoes.innerHTML = print;
 }
 
-function printItensFiltrados(itens){
-  let itensFiltrados = document.getElementById("printItensFiltrados")
+function printItensFiltrados(itens) {
+  let itensFiltrados = document.getElementById("printItensFiltrados");
   itensFiltrados.innerHTML = `
   <div style="max-width: 640px;">
     <table id="listaFiltrada" style="width: 100%; border-collapse: collapse; margin-top: 10px;">
@@ -71,36 +76,45 @@ function printItensFiltrados(itens){
           <th style=" text-align: left; padding: 8px;">Nome</th>
           <th style=" text-align: left; padding: 8px;">Preço de Venda</th>
           <th style=" text-align: left; padding: 8px;">Volume</th>
+          <th style=" text-align: left; padding: 8px;">Oferta Total</th>
         </tr>
     </div>
-  `
-    let tabelaFiltrada = document.getElementById('listaFiltrada')
+  `;
+  let tabelaFiltrada = document.getElementById("listaFiltrada");
 
+  for (let i = 0; i < itens.length; i++) {
+    const nomeDecodificado = decodeURIComponent(itens[i].name);
+    const preco = itens[i].precoDeVenda;
+    const volume = parseInt(itens[i].volumeMedioPorHora);
+    const ofertaTotal = itens[i].ofertasQuant
+   
 
-  for(let i =0;i<itens.length;i++){
-    const nomeDecodificado = decodeURIComponent(itens[i].name)
-    const preco = itens[i].precoDeVenda
-    const volume = itens[i].volumeMedioPorHora
-    const volumeTratado = volume.toFixed(2)
-
-     tabelaFiltrada.innerHTML += `
+    tabelaFiltrada.innerHTML += `
     <tr>
      <td style=" text-align: left; padding: 8px;"><a href="${itens[i].link}" target="_blank">${nomeDecodificado}</a></td>
      <td style=" text-align: left; padding: 8px;">${preco}</td>
-     <td style=" text-align: left; padding: 8px;">${volumeTratado}</td>
+     <td style=" text-align: left; padding: 8px;">${volume}</td>
+     <td style=" text-align: left; padding: 8px;">${ofertaTotal}</td>
    </tr>
-     `
-  } 
+     `;
+  }
 }
 
 function capturarDados() {
-botaoFiltro.removeEventListener('click', capturarDados);
-let quantidadeItens = parseInt(quantItens.value);
-let ofertasMinimas = parseInt(valorMinimo.value)
-let intervaloHora = parseInt(intervaloDeHoras.value)
-let volumeMedio = parseInt(volumeMedioPorHora.value)
-let cotacaoMinima = parseFloat(precoMinimo.value)
-let cotacaoMaxima = parseFloat(precoMaximo.value)
-config(quantidadeItens,ofertasMinimas,intervaloHora,volumeMedio,cotacaoMinima,cotacaoMaxima)
-converteStringEmCotacaoEIDBook();
+  botaoFiltro.removeEventListener("click", capturarDados);
+  let quantidadeItens = parseInt(quantItens.value);
+  let ofertasMinimas = parseInt(valorMinimo.value);
+  let intervaloHora = parseInt(intervaloDeHoras.value);
+  let volumeMedio = parseInt(volumeMedioPorHora.value);
+  let cotacaoMinima = parseFloat(precoMinimo.value);
+  let cotacaoMaxima = parseFloat(precoMaximo.value);
+  config(
+    quantidadeItens,
+    ofertasMinimas,
+    intervaloHora,
+    volumeMedio,
+    cotacaoMinima,
+    cotacaoMaxima
+  );
+  converteStringEmCotacaoEIDBook();
 }
