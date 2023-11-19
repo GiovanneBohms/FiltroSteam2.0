e945ec308237dec6ddde8ccef5d9a0f52d3c61a2
giovannebohms <giovannebohms@gmail.com>
2023-11-19 16:16:59 -0300

    book encomenda e oferta

--- src/book.js
+++ src/book.js
@@ -1,8 +1,24 @@
 async function coletaBook(){
   const itens = await coletaCotacoes()
-  const book = await coletaOfertaEncomendaDoBook(itens[0].idBook)
-  console.log(book.buy_order_graph)
-  console.log(book.sell_order_graph)
+  let i = 0;
+  let d=0
+  while(i < itens.length){
+    const book = await coletaOfertaEncomendaDoBook(itens[i].idBook)
 
+    if(!book){
+      d++
+      console.log(`Um delay de ${d} minutos foi aplicado a API do book`)
+      await delay(1000*d)
+      continue;
+    }
 
+    const ofertasEncomendas = {
+    encomendas: book.buy_order_graph,
+    ofertas: book.sell_order_graph
+    }
+    itens[i].setBook(ofertasEncomendas)
+    console.log(itens[i].name,itens[i].book)
+    i++
+  }
+  console.log("Books coletados",itens.length)
 }
