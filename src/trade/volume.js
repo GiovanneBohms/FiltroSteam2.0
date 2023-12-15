886b37d90cf6c4b5b542ad9121290acae5d77543
giovannebohms <giovannebohms@gmail.com>
2023-12-15 16:17:12 -0300

    venderAlvoCalculo(

--- src/trade/volume.js
+++ src/trade/volume.js
@@ -1,5 +1,33 @@
-function volumeMedioReal(bookEncomenda){
+console.log("./src/trade/volume.js")
 
-  console.log("volumeMedioReal",bookEncomenda)
+function volumeMedioReal(cotacoes){
+  let volumeTotal = 0;
+  let volumeMedioHora = 0;
+
+  cotacoes.forEach(cotacao => {
+    volumeTotal += cotacao[1]
+  });
+  volumeMedioHora = parseInt(volumeTotal/cotacoes.length)
+  console.log(volumeMedioHora)
+  const descrepancias = cotacoes.filter(cotacao => cotacao[1] >= volumeMedioHora*1)
+  console.log(descrepancias)
+  descrepancias.forEach(cotacao => {
+    volumeTotal = volumeTotal - cotacao[1]
+  });
+
+  const volumeMedioHoraReal = parseInt(volumeTotal/(cotacoes.length - descrepancias.length))
+
+  console.log(volumeMedioHoraReal)
   
+}
+
+function venderAlvoCalculo(bookOferta,volumeMedioHora,bookEncomenda){
+
+  if(volumeMedioHora >  bookOferta[(bookOferta.length)-1][1]){
+    console.log(bookOferta, bookEncomenda)
+    return ""
+  }
+  const venderAlvo = (bookOferta.find(item => item[1] >= volumeMedioHora))[0];
+  return venderAlvo
+
 }
